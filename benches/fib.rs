1a951902bd398ef44b9246b49a901fac79e0e0ae
Lloyd <lloydmeta@gmail.com>
2016-11-06 20:59:49 +0900

    Higher fib for benches

--- benches/fib.rs
+++ benches/fib.rs
@@ -8,12 +8,12 @@ use fibonacci::maths::fib::{ fib_at_index, Memoed };
 use test::Bencher;
 
 #[bench]
-fn bench_fib30(b: &mut Bencher) {
-    b.iter(|| { fib_at_index(30) });
+fn bench_fib(b: &mut Bencher) {
+    b.iter(|| { fib_at_index(10000) });
 }
 
 #[bench]
-fn bench_fib30_memoed(b: &mut Bencher) {
+fn bench_fib_memoed(b: &mut Bencher) {
     let memoed = Memoed::new();
-    b.iter(|| { memoed.at_index(30)})
+    b.iter(|| { memoed.at_index(10000)})
 }
