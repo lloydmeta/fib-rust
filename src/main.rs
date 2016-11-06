3eda66d223507fb7f77623f509983b0a2b31a654
Lloyd <lloydmeta@gmail.com>
2016-11-06 15:45:06 +0900

    Add interactive mode

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
+extern crate num;
 extern crate fibonacci;
 
 use std::io;
-use std::num;
+use std::num::ParseIntError;
+use std::env::args;
 use std::io::stdin;
-use fibonacci::maths::fib::fib_at_index;
+
+use num::bigint::BigUint;
+use fibonacci::maths::fib::{ Memoed, fib_at_index };
 
 fn main() {
-    engage_user().unwrap();
+    let args: Vec<String> = args().collect();
+    // First argument is usually the path of the executable
+    if let Some(s) = args.get(1) {
+        one_off_mod(s.trim().to_owned())
+    } else {
+        interactive_mode()
+    }.unwrap()
 }
 
-fn engage_user() -> Result<(), CliError> {
-    println!("Which Fibonacci index are you interested in?");
-    let input = try!(read_stdio());
+fn one_off_mod(input: String) ->  Result<(), CliError> {
     let index = try!(parse_positive_num(input));
     let fib_value = fib_at_index(index);
-    Ok(println!("Fib at {} is {}", index, fib_value))
+    Ok(print_result(index, fib_value))
+}
+
+fn interactive_mode() -> Result<(), CliError> {
+    println!("\n**************** Interactive Mode ********************");
+    println!(" ctrl+c or ctrl+d or enter any invalid number to exit ");
+    println!("******************************************************");
+    let fib_gen = Memoed::new();
+    loop {
+        println!("Which Fibonacci index are you interested in?");
+        let input = try!(read_stdio());
+        let index = try!(parse_positive_num(input));
+        let fib_value = fib_gen.at_index(index);
+        print_result(index, fib_value)
+    }
+}
+
+fn print_result(idx: usize, fib: BigUint) {
+    println!("Fibonacci[{}] is {}", idx, fib)
 }
 
 fn parse_positive_num(s: String) -> Result<usize, CliError> {
@@ -37,5 +63,5 @@ fn read_stdio() -> Result<String, CliError> {
 #[derive(Debug)]
 enum CliError {
     Io(io::Error),
-    Parse(num::ParseIntError),
+    Parse(ParseIntError),
 }
