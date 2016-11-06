c41822753b67204e8860df7e268c53a13d904df7
Lloyd <lloydmeta@gmail.com>
2016-11-06 15:08:09 +0900

    Simplify the RW lock code

--- src/maths/fib.rs
+++ src/maths/fib.rs
@@ -11,7 +11,7 @@ use num::traits::{Zero, One};
 /// # use num::bigint::ToBigUint;
 /// # use self::fibonacci::maths::fib::fib_at_index;
 /// # fn main() {
-/// assert!(fib_at_index(3) == 2.to_biguint().unwrap());
+/// assert_eq!(fib_at_index(3), 2.to_biguint().unwrap());
 /// # }
 /// ```
 pub fn fib_at_index(i: usize) -> BigUint {
@@ -26,11 +26,11 @@ pub fn fib_at_index(i: usize) -> BigUint {
 // Called when index is greater than 1
 fn fib_non_trivial(i: usize) -> BigUint {
     // Rust does not have TOC so we use mutable refs to get around stack overflow problems
-    let mut acc_prev: BigUint = One::one();
-    let mut acc: BigUint = One::one();
+    let mut acc_prev = <BigUint as One>::one();
+    let mut acc = <BigUint as One>::one();
     for _ in 2..i {
         let next_prev = acc.clone();
-        acc = acc + acc_prev;
+        acc = acc + &acc_prev;
         acc_prev = next_prev;
     }
     acc
@@ -41,6 +41,20 @@ pub struct Memoed {
 }
 
 impl Memoed {
+    /// Creates a new Memoed for you to use
+    ///
+    /// # Example
+    /// ```
+    /// # extern crate num;
+    /// # extern crate fibonacci;
+    /// # use num::bigint::ToBigUint;
+    /// # use self::fibonacci::maths::fib::Memoed;
+    /// # fn main() {
+    /// let mut fib_gen = Memoed::new();
+    /// assert_eq!(fib_gen.at_index(3), 2.to_biguint().unwrap());
+    /// # }
+    ///
+    /// ```
     pub fn new() -> Memoed {
         let mut new_cache = Vec::with_capacity(1000);
         let min_cache = &mut vec![Zero::zero(), One::one()];
@@ -48,33 +62,46 @@ impl Memoed {
         Memoed { cache: RwLock::new(new_cache) }
     }
 
+    /// Returns the Fibonacci value at the given value
+    ///
+    /// Internally uses a thread-safe cache backed by a double-checked RWLock
+    ///
+    /// # Example
+    /// ```
+    /// # extern crate num;
+    /// # extern crate fibonacci;
+    /// # use num::bigint::ToBigUint;
+    /// # use self::fibonacci::maths::fib::Memoed;
+    /// # fn main() {
+    /// let mut fib_gen = Memoed::new();
+    /// assert_eq!(fib_gen.at_index(3), 2.to_biguint().unwrap());
+    /// # }
+    /// ```
     pub fn at_index(&self, to: usize) -> BigUint {
-        let mut ret: BigUint = Zero::zero();
-        let mut found_cached = false;
         {
             // First try to acquire a read lock and read from the cache
-            if let Ok(data) = self.cache.read() {
-                if data.len() > to {
-                    ret = data[to].clone();
-                    found_cached = true;
-                }
+            // For some reason, moving this into the match causes the lock
+            // to never move out of scope, causing use todeadlock when trying to write
+            let lock_result =  self.cache.read();
+            match lock_result {
+                Ok(ref data) if data.len() > to => Some(data[to].clone()),
+                _ => None
             }
-        }
-        if !found_cached {
-            // Unable to retrieve from the cache, so we need to grab a write lock and start generating
+        }.unwrap_or_else(|| {
+            // Unable to retrieve from the cache, so we need to grab a write lock and
+            // start generating
             let mut data = self.cache.write().unwrap();
-            // Check one more time in case work was done in another thread while we were acquiring the
-            // write lock
+            // Check one more time in case work was done in another thread while we were
+            // acquiring the write lock
             if data.len() > to {
-                ret = data[to].clone()
+                data[to].clone()
             } else {
                 for current_max in data.len()..to + 1 {
-                    let sum = data[current_max - 1].clone() + &data[current_max - 2];
+                    let sum = &data[current_max - 1] + &data[current_max - 2];
                     data.push(sum);
                 }
-                ret = data[to].clone()
+                data[to].clone()
             }
-        }
-        ret
+        })
     }
 }
