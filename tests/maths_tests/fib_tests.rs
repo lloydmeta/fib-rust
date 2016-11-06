840ec34d8f76b7498b8fa64a1bcd3daa1a8df445
Lloyd <lloydmeta@gmail.com>
2016-11-06 09:23:38 +0900

    Add a thread-safe memoed fibber

--- tests/maths_tests/fib_tests.rs
+++ tests/maths_tests/fib_tests.rs
@@ -1,5 +1,8 @@
 extern crate fibonacci;
-use self::fibonacci::maths::fib::fib_at_index;
+
+use self::fibonacci::maths::fib::{ fib_at_index, Memoed };
+use std::thread;
+use std::sync::Arc;
 
 #[test]
 fn test_fib0() {
@@ -21,6 +24,26 @@ fn test_fib_after_1() {
         current = fib_at_index(i);
         assert_eq!(current, (prev + next_prev));
         prev = next_prev;
-        i = i + 1;
+        i += 1;
+    }
+}
+
+#[test]
+fn test_memoed() {
+    let memoed = Memoed::new();
+    assert_eq!(memoed.at_index(10), 55)
+}
+
+#[test]
+fn test_memoed_multithreaded() {
+    let memoed = Arc::new(Memoed::new());
+    for _ in 0..10 {
+        let memoed = memoed.clone();
+        let _ = thread::spawn(move || {
+            for i in 0 .. 30 {
+                let f = memoed.at_index(i);
+                assert_eq!(f, fib_at_index(i as i64));
+            }
+        });
     }
 }
