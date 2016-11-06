f6540a70d9dde256c523027640751f3e49a91b88
Lloyd <lloydmeta@gmail.com>
2016-11-06 09:27:22 +0900

    Add a benchmark

--- benches/fib.rs
+++ benches/fib.rs
@@ -4,10 +4,16 @@ extern crate fibonacci;
 
 extern crate test;
 
-use fibonacci::maths::fib::fib_at_index;
+use fibonacci::maths::fib::{ fib_at_index, Memoed };
 use test::Bencher;
 
 #[bench]
 fn bench_fib30(b: &mut Bencher) {
-    b.iter(|| { fib_at_index(30)});
+    b.iter(|| { fib_at_index(30) });
+}
+
+#[bench]
+fn bench_fib30_memoed(b: &mut Bencher) {
+    let memoed = Memoed::new();
+    b.iter(|| { memoed.at_index(30)})
 }
